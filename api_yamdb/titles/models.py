10e9b3a2e7b62be0955ae4a178b408ea0697488a
Дмитрий Прибавкин <Pribavkin.dmitrij@yandex.ru>
2023-02-27 22:58:07 +0500

    First commit

---
+++ api_yamdb/titles/models.py
@@ -0,0 +1,40 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=50, unique=True, db_index=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=50, unique=True, db_index=True)
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=256)
+    # year =
+    description = models.TextField(max_length=256, blank=True)
+    genre = models.ManyToManyField(
+        Genre, related_name='titles',
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL,
+        related_name='titles', null=True, blank=True
+    )
+
+
