612d216a2c1bb5b373b91147f05d9928c4f7bfad
Дмитрий Прибавкин <Pribavkin.dmitrij@yandex.ru>
2023-02-28 13:40:34 +0500

    Second commit

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -38,3 +38,37 @@ class Title(models.Model):
     )
 
 
+class Reviews(models.Model):
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE, related_name='reviews'
+    )
+    text = models.TextField(max_length=256)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reviews'
+    )
+    # score =
+    pub_date = models.DateTimeField(
+        auto_now_add=True, db_index=True
+    )
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+
+    def __str__(self):
+        return self.text
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments',
+    )
+    text = models.TextField(max_length=256)
+    reviews = models.ForeignKey(
+        Reviews, on_delete=models.CASCADE, related_name='comments',
+    )
+    pub_date = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
