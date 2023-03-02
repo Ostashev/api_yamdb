350c4d01fadd29ec54d0d9b45218cb19d31efe0b
Дмитрий Прибавкин <Pribavkin.dmitrij@yandex.ru>
2023-03-02 20:35:05 +0500

    the model is ready

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -27,7 +27,7 @@ class Genre(models.Model):
 
 class Title(models.Model):
     name = models.CharField(max_length=256)
-    # year =
+    year = models.IntegerField('год')
     description = models.TextField(max_length=256, blank=True)
     genre = models.ManyToManyField(
         Genre, related_name='titles',
@@ -36,39 +36,3 @@ class Title(models.Model):
         Category, on_delete=models.SET_NULL,
         related_name='titles', null=True, blank=True
     )
-
-
-class Reviews(models.Model):
-    title = models.ForeignKey(
-        Title, on_delete=models.CASCADE, related_name='reviews'
-    )
-    text = models.TextField(max_length=256)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='reviews'
-    )
-    # score =
-    pub_date = models.DateTimeField(
-        auto_now_add=True, db_index=True
-    )
-
-    class Meta:
-        verbose_name = 'Отзыв'
-        verbose_name_plural = 'Отзывы'
-
-    def __str__(self):
-        return self.text
-
-
-class Comment(models.Model):
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments',
-    )
-    text = models.TextField(max_length=256)
-    reviews = models.ForeignKey(
-        Reviews, on_delete=models.CASCADE, related_name='comments',
-    )
-    pub_date = models.DateTimeField(auto_now_add=True, db_index=True)
-
-    class Meta:
-        verbose_name = 'Комментарий'
-        verbose_name_plural = 'Комментарии'
