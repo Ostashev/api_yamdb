fa400bd4cbb7ebc1a18d0d9d48bd3903ebb89481
Дмитрий Прибавкин <Pribavkin.dmitrij@yandex.ru>
2023-03-04 16:09:26 +0500

    ready

---
+++ api_yamdb/api/mixins.py
@@ -0,0 +1,9 @@
+from rest_framework import mixins, viewsets
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import (CreateModelMixin, DestroyModelMixin,
+                                   ListModelMixin)
+
+
+class ModelMixinSet(CreateModelMixin, ListModelMixin,
+                    DestroyModelMixin, GenericViewSet):
+    pass
